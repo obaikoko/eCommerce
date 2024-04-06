fbceb9000c8d1d1a06de952b37da0ec370d22e2f
obaikoko <jesseobinna7@gmail.com>
2024-04-06 18:11:08 +0100

    updated

--- frontend/src/features/slices/apiSlice.js
+++ frontend/src/features/slices/apiSlice.js
@@ -1,8 +1,8 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const baseQuery = fetchBaseQuery({
-  // baseUrl: 'https://proshop-mpna.onrender.com/',
-  baseUrl: 'http://localhost:5000/',
+  baseUrl: 'https://proshop-mpna.onrender.com/',
+  // baseUrl: 'http://localhost:5000/',
 });
 
 export const apiSlice = createApi({
